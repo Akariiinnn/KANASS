3ed249a5d3780d9eeb49422639d84719beee4f36
niraka <robert.quent@gmail.com>
2026-02-02 16:36:58 +0100

    macOS branch and support

---
+++ cmake-build-debug/CMakeFiles/KANASS.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KANASS.dir/KanaGame.cpp.o"
+  "CMakeFiles/KANASS.dir/KanaGame.cpp.o.d"
+  "CMakeFiles/KANASS.dir/kana.cpp.o"
+  "CMakeFiles/KANASS.dir/kana.cpp.o.d"
+  "CMakeFiles/KANASS.dir/main.cpp.o"
+  "CMakeFiles/KANASS.dir/main.cpp.o.d"
+  "KANASS"
+  "KANASS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KANASS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
